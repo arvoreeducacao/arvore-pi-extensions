f1a348808b4ee806f1a91b498f8e7e40d264630d
Jott4 <jvgcunha2002@gmail.com>
2026-06-24 16:13:14 -0300

    remove slack-agent package

--- packages/slack-agent/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { loadConfig, type AgentConfig } from "./config.js";
-export { createAgent } from "./agent.js";
-export { SessionManager, Session } from "./session-manager.js";
-export { RpcClient } from "./rpc-client.js";
